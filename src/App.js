2d49c59f632fd48b5e899ceddfa8ed48fb388c00
iamwebartdevteam <biswajit@webart.technology>
2022-10-21 23:15:01 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import "../src/assets/css/style.css";
 import AppRouter from "./router/AppRouter";
 import "react-toastify/dist/ReactToastify.css";
 import 'react-responsive-modal/styles.css';
+import {
+  Elements,
+  CardElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { useState } from "react";
+
 function App() {
+
+  // const [isPaymentLoading, setPaymentLoading] = useState(false);
+  // const stripe = useStripe();
+  // const elements = useElements();
+
+  // const clientSecret = getClientSecret();
+
   return <AppRouter />;
 }
 
