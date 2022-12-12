bfe466c5bb0cd68f513fbe6adcbeebb6a5c0dfd5
iamwebartdevteam <biswajit@webart.technology>
2022-12-12 09:43:49 +0530

    term-and-conditons page contant update

--- src/pages/TermsAndConditions.js
+++ src/pages/TermsAndConditions.js
@@ -14,188 +14,54 @@ const TermsAndConditions = () => {
             <div className="col-md-8">
               <main>
                 <article class="terms-and-conditions">
-                  <h1>Terms and Conditions for Supply We Stock LLC</h1>
-                  <h2>Introduction</h2>
+                  <h1 className="mb-5">
+                    Terms and Conditions for Supply We Stock LLC
+                  </h1>
+                  {/* <h2>Introduction</h2> */}
                   <p>
-                    These Website Standard Terms and Conditions written on this
-                    webpage shall manage your use of our website,
-                    supplywestock.com/ accessible at https://supplywestock.com/.
+                    Thank you for considering using our website to sell your
+                    products! By using our website, you agree to the following
+                    terms and conditions:
                   </p>
                   <p>
-                    These Terms will be applied fully and affect to your use of
-                    this Website. By using this Website, you agreed to accept
-                    all terms and conditions written in here. You must not use
-                    this Website if you disagree with any of these Website
-                    Standard Terms and Conditions. These Terms and Conditions
-                    have been generated with the help of the{" "}
-                    <a href="https://www.termsandcondiitionssample.com/">
-                      Terms And Conditiions Sample Generator
-                    </a>
-                    .
+                    You are responsible for ensuring that the products you list
+                    on our website are accurate and complete, and that you have
+                    the right to sell them .
                   </p>
                   <p>
-                    Minors or people below 18 years old are not allowed to use
-                    this Website.
+                    You are responsible for setting the price of your products
+                    and for managing the sale of your products and fulfilling
+                    orders.
                   </p>
-                  <h2>Intellectual Property Rights</h2>
+                  {/* <h2>Intellectual Property Rights</h2> */}
                   <p>
-                    Other than the content you own, under these Terms, Supply We
-                    Stock LLC and/or its licensors own all the intellectual
-                    property rights and materials contained in this Website.
+                    We are not responsible for any disputes that may arise
+                    between you and a buyer.
                   </p>
                   <p>
-                    You are granted limited license only for purposes of viewing
-                    the material contained on this Website.
+                    We reserve the right to change these terms and conditions at
+                    any time without notice.
                   </p>
-                  <h2>Restrictions</h2>
+                  {/* <h2>Restrictions</h2> */}
                   <p>
-                    You are specifically restricted from all of the following:
-                  </p>
-                  <ul className="tremUl">
-                    <li>publishing any Website material in any other media;</li>
-
-                    <li>
-                      selling, sublicensing and/or otherwise commercializing any
-                      Website material;
-                    </li>
-
-                    <li>
-                      publicly performing and/or showing any Website material;
-                    </li>
-
-                    <li>
-                      using this Website in any way that is or may be damaging
-                      to this Website;
-                    </li>
-
-                    <li>
-                      using this Website in any way that impacts user access to
-                      this Website;
-                    </li>
-
-                    <li>
-                      using this Website contrary to applicable laws and
-                      regulations, or in any way may cause harm to the Website,
-                      or to any person or business entity;
-                    </li>
-
-                    <li>
-                      engaging in any data mining, data harvesting, data
-                      extracting or any other similar activity in relation to
-                      this Website;
-                    </li>
-
-                    <li>
-                      using this Website to engage in any advertising or
-                      marketing.
-                    </li>
-                  </ul>
-                  <p>
-                    Certain areas of this Website are restricted from being
-                    access by you and Supply We Stock LLC may further restrict
-                    access by you to any areas of this Website, at any time, in
-                    absolute discretion. Any user ID and password you may have
-                    for this Website are confidential and you must maintain
-                    confidentiality as well.
-                  </p>
-                  <h2>Your Content</h2>
-                  <p>
-                    In these Website Standard Terms and Conditions, "Your
-                    Content" shall mean any audio, video text, images or other
-                    material you choose to display on this Website. By
-                    displaying Your Content, you grant Supply We Stock LLC a
-                    non-exclusive, worldwide irrevocable, sub licensable license
-                    to use, reproduce, adapt, publish, translate and distribute
-                    it in any and all media.
+                    By using our website, you agree to indemnify and hold us
+                    harmless from any claims, losses, or damages arising from
+                    your use of our website or your violation of these terms and
+                    conditions.
                   </p>
 
                   <p>
-                    Your Content must be your own and must not be invading any
-                    third-party’s rights. Supply We Stock LLC reserves the right
-                    to remove any of Your Content from this Website at any time
-                    without notice.
+                    These terms and conditions constitute the entire agreement
+                    between you and us, and supersede any prior agreements or
+                    understandings, whether oral or written.
                   </p>
-
-                  <h2>Your Privacy</h2>
-
-                  <p>Please read Privacy Policy.</p>
-
-                  <h2>No warranties</h2>
-
+                  {/* <h2>Your Content</h2> */}
                   <p>
-                    This Website is provided "as is," with all faults, and
-                    Supply We Stock LLC express no representations or
-                    warranties, of any kind related to this Website or the
-                    materials contained on this Website. Also, nothing contained
-                    on this Website shall be interpreted as advising you.
+                    If any provision of these terms and conditions is found to
+                    be invalid or unenforceable, the remaining provisions will
+                    remain in full force and effect.
                   </p>
-
-                  <h2>Limitation of liability</h2>
-
-                  <p>
-                    In no event shall Supply We Stock LLC, nor any of its
-                    officers, directors and employees, shall be held liable for
-                    anything arising out of or in any way connected with your
-                    use of this Website whether such liability is under
-                    contract. Supply We Stock LLC, including its officers,
-                    directors and employees shall not be held liable for any
-                    indirect, consequential or special liability arising out of
-                    or in any way related to your use of this Website.
-                  </p>
-
-                  <h2>Indemnification</h2>
-
-                  <p>
-                    You hereby indemnify to the fullest extent Supply We Stock
-                    LLC from and against any and/or all liabilities, costs,
-                    demands, causes of action, damages and expenses arising in
-                    any way related to your breach of any of the provisions of
-                    these Terms.
-                  </p>
-
-                  <h2>Severability</h2>
-
-                  <p>
-                    If any provision of these Terms is found to be invalid under
-                    any applicable law, such provisions shall be deleted without
-                    affecting the remaining provisions herein.
-                  </p>
-
-                  <h2>Variation of Terms</h2>
-
-                  <p>
-                    Supply We Stock LLC is permitted to revise these Terms at
-                    any time as it sees fit, and by using this Website you are
-                    expected to review these Terms on a regular basis.
-                  </p>
-
-                  <h2>Assignment</h2>
-
-                  <p>
-                    The Supply We Stock LLC is allowed to assign, transfer, and
-                    subcontract its rights and/or obligations under these Terms
-                    without any notification. However, you are not allowed to
-                    assign, transfer, or subcontract any of your rights and/or
-                    obligations under these Terms.
-                  </p>
-
-                  <h2>Entire Agreement</h2>
-
-                  <p>
-                    These Terms constitute the entire agreement between Supply
-                    We Stock LLC and you in relation to your use of this
-                    Website, and supersede all prior agreements and
-                    understandings.
-                  </p>
-
-                  <h2>Governing Law & Jurisdiction</h2>
-
-                  <p>
-                    These Terms will be governed by and interpreted in
-                    accordance with the laws of the State of us, and you submit
-                    to the non-exclusive jurisdiction of the state and federal
-                    courts located in us for the resolution of any disputes.
-                  </p>
+                  <p>Thank you for using our website!</p>
                 </article>
               </main>
             </div>
